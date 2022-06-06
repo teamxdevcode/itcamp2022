b9465abe64aaa6d576780acf9ec175df45660ffe
suphafang <suphachai.fang@gmail.com>
2022-06-06 08:06:16 +0700

    Validation error messages is being translated.

--- app/Http/Livewire/CreateUserForm.php
+++ app/Http/Livewire/CreateUserForm.php
@@ -34,64 +34,75 @@ class CreateUserForm extends Component
 	];
 
   public $formData = [
-      'name' => null,
-      'surname' => null,
-      'nickname' => null,
-      'birth' => null,
-      'gender' => null,
-      'blood_type' => null,
-      'religion' => null,
-      'address' => null,
-      'province' => null,
-      'district' => null,
-      'subdistrict' => null,
-      'phone' => null,
-      'email' => null,
-      'education_level' => null,
-      'school' => null,
-      'education_program' => null,
-      'education_certificate' => null,
-      'has_congenital_disease' => '0',
-      'congenital_disease_detail' => '',
-      'has_allergic' => '0',
-      'allergic_detail' => '',
-      'shirt_size' => null,
-      'activities_detail' => null,
-      'emergency_contact_name' => null,
-      'emergency_contact_surname' => null,
-      'emergency_contact_phone' => null,
-      'emergency_contact_relationship' => null,
+		'name' => null,
+		'surname' => null,
+		'nickname' => null,
+		'birth' => null,
+		'gender' => null,
+		'blood_type' => null,
+		'religion' => null,
+		'address' => null,
+		'province' => null,
+		'district' => null,
+		'subdistrict' => null,
+		'phone' => null,
+		'email' => null,
+		'education_level' => null,
+		'school' => null,
+		'education_program' => null,
+		'education_certificate' => null,
+		'has_congenital_disease' => '0',
+		'congenital_disease_detail' => '',
+		'has_allergic' => '0',
+		'allergic_detail' => '',
+		'shirt_size' => null,
+		'activities_detail' => null,
+		'emergency_contact_name' => null,
+		'emergency_contact_surname' => null,
+		'emergency_contact_phone' => null,
+		'emergency_contact_relationship' => null,
     ];
 
-  protected $messages = [
-    'formData.name.required' => 'กรุณาระบุชื่อ',
-    'formData.surname.required' => 'กรุณาระบุนามสกุล',
-    'formData.nickname.required' => 'กรุณาระบุชื่อเล่น',
-    'formData.birth.required' => 'กรุณาระบุวันเกิด',
-    'formData.gender.required' => 'กรุณาระบุเพศ',
-    'formData.blood_type.required' => 'กรุณาระบุกรุ๊ปเลือด',
-    'formData.religion.required' => 'กรุณาระบุศาสนา',
-    'formData.address.required' => 'กรุณาระบุที่อยู่ปัจจุบัน',
-    'formData.province.required' => 'กรุณาระบุจังหวัดที่อยู่ปัจจุบัน',
-    'formData.district.required' => 'กรุณาระบุอำเภอ/เขตที่อยู่ปัจจุบัน',
-    'formData.subdistrict.required' => 'กรุณาระบุตำบล/แขวงที่อยู่ปัจจุบัน',
-    'formData.phone.required' => 'กรุณาระบุเบอร์โทรศัพท์ของคุณ',
-    'formData.email.required' => 'กรุณาระบุอีเมลของคุณ',
-    'formData.education_level.required' => 'กรุณาระบุระดับการศึกษาปัจจุบัน',
-    'formData.school.required' => 'กรุณาระบุโรงเรียน/สถานศึกษาปัจจุบัน',
-    'formData.education_program.required' => 'กรุณาระบุแผนการเรียน/สาขา',
-    'education_certificate.required' => 'กรุณาอัพโหลดเอกสาร ปพ. 7',
-    'formData.has_congenital_disease.required' => 'กรุณาระบุโรคประจำตัว',
-    'formData.congenital_disease_detail.required' => 'กรุณาระบุรายละเอียดโรค',
-    'formData.has_allergic.required' => 'กรุณาระบุสิ่งที่แพ้/อาหารที่แพ้',
-    'formData.allergic_detail.required' => 'กรุณาระบุสิ่งที่แพ้',
-    'formData.shirt_size.required' => 'กรุณาระบุไซส์เสื้อ',
-    'formData.activities_detail.required' => 'กรุณาระบุกิจกรรมที่เข้าร่วมหรือผลงานที่เคยทำ หรือผลงานที่อยากนำเสนอ',
-    'formData.emergency_contact_name.required' => 'กรุณาระบุชื่อจริงของผู้ปกครอง',
-    'formData.emergency_contact_surname.required' => 'กรุณาระบุนามสกุลของผู้ปกครอง',
-    'formData.emergency_contact_phone.required' => 'กรุณาระบุเบอร์โทรศัพท์ของผู้ปกครอง',
-    'formData.emergency_contact_relationship.required' => 'กรุณาระบุความเกี่ยวข้องกับผู้ปกครอง',
-  ];
+	protected $rules = [
+	  'formData.*' => 'required',
+	  'formData.birth' => 'required|date',
+	  'formData.phone' => 'required|digits:10',
+	  'formData.email' => 'required|email',
+	  'formData.education_certificate' => 'required|image|max:2048',
+	  'formData.congenital_disease_detail' => '',
+	  'formData.allergic_detail' => '',
+	  'formData.emergency_contact_phone' => 'required|digits:10',
+	];
+
+	protected $validationAttributes = [
+		'formData.name' => 'ชื่อจริง',
+		'formData.surname' => 'นามสกุล',
+		'formData.nickname' => 'ชื่อเล่น',
+		'formData.birth' => 'วันเกิด',
+		'formData.blood_type' => 'หมู่เลือด',
+		'formData.religion' => 'ศาสนา',
+		'formData.address' => 'ที่อยู่ปัจจุบัน',
+		'formData.province' => 'จังหวัดที่อยู่ปัจจุบัน',
+		'formData.district' => 'อำเภอ/เขตที่อยู่ปัจจุบัน',
+		'formData.subdistrict' => 'ตำบล/แขวงที่อยู่ปัจจุบัน',
+		'formData.phone' => 'เบอร์โทรศัพท์',
+		'formData.email' => 'อีเมล',
+		'formData.education_level' => 'ระดับการศึกษาปัจจุบัน',
+		'formData.school' => 'โรงเรียน/สถานศึกษาปัจจุบัน',
+		'formData.education_program' => 'แผนการเรียน/สาขาปัจจุบัน',
+		'formData.education_certificate' => 'ใบรับรองผลการศึกษา (ปพ.7) ',
+		'education_certificate' => 'ใบรับรองผลการศึกษา (ปพ.7) ',
+		'formData.has_congenital_disease' => 'โรคประจำตัว',
+		'formData.congenital_disease_detail' => 'รายละเอียดโรคประจำตัว',
+		'formData.has_allergic' => 'สิ่งที่แพ้/อาหารที่แพ้',
+		'formData.allergic_detail' => 'รายละเอียดสิ่งที่แพ้/อาหารที่แพ้',
+		'formData.shirt_size' => 'ไซส์เสื้อ',
+		'formData.activities_detail' => 'กิจกรรมที่เข้าร่วมหรือผลงานที่เคยทำ หรือผลงานที่อยากนำเสนอ',
+		'formData.emergency_contact_name' => 'ชื่อจริงของผู้ติดต่อฉุกเฉินหรือผู้ปกครอง',
+		'formData.emergency_contact_surname' => 'นามสกุลของผู้ติดต่อฉุกเฉินหรือผู้ปกครอง',
+		'formData.emergency_contact_phone' => 'เบอร์โทรศัพท์ของผู้ติดต่อฉุกเฉินหรือผู้ปกครอง',
+		'formData.emergency_contact_relationship' => 'ความเกี่ยวข้องของผู้ติดต่อฉุกเฉินหรือผู้ปกครอง',
+	];
 
   public $photo_validation = false;
   public $education_certificate;
@@ -99,32 +110,22 @@ class CreateUserForm extends Component
   public function updated($propertyName)
   {
     if($propertyName === 'education_certificate') {
-      $this->photo_validation = false;
-      $this->validateOnly($propertyName, [
-        'education_certificate' => 'required|image|max:2048',
-      ]);
-      $this->photo_validation = true;
+		$this->photo_validation = false;
+		$this->validateOnly($propertyName, ['education_certificate' => 'required|image|max:2048']);
+		$this->photo_validation = true;
     } else {
-      $validate = [
-        'formData.*' => 'required',
-        'formData.birth' => 'required|date',
-        'formData.phone' => 'required|digits:10',
-        'formData.email' => 'required|email',
-        'formData.education_certificate' => '',
-        'formData.congenital_disease_detail' => '',
-        'formData.allergic_detail' => '',
-        'formData.emergency_contact_phone' => 'required|digits:10',
-      ];
+      $this->rules['formData.congenital_disease_detail'] = '';
+      $this->rules['formData.allergic_detail'] = '';
 
       if ($this->formData['has_congenital_disease']) {
-        $validate['formData.congenital_disease_detail'] = 'required';
+        $this->rules['formData.congenital_disease_detail'] = 'required';
       }
 
       if ($this->formData['has_allergic']) {
-        $validate['formData.allergic_detail'] = 'required';
+        $this->rules['formData.allergic_detail'] = 'required';
       }
 
-      $this->validateOnly($propertyName, $validate);
+      	$this->validateOnly($propertyName);
     }
   }
 
@@ -142,25 +143,18 @@ class CreateUserForm extends Component
   }
 
   public function save() {
-    $validate = [
-      'formData.*' => 'required',
-      'formData.birth' => 'required|date',
-      'formData.phone' => 'required|digits:10',
-      'formData.email' => 'required|email',
-      'formData.education_certificate' => '',
-      'formData.congenital_disease_detail' => '',
-      'formData.allergic_detail' => '',
-      'formData.emergency_contact_phone' => 'required|digits:10',
-    ];
+    $this->rules['formData.congenital_disease_detail'] = '';
+    $this->rules['formData.allergic_detail'] = '';
 
     if ($this->formData['has_congenital_disease']) {
-      $validate['formData.congenital_disease_detail'] = 'required';
+      $this->rules['formData.congenital_disease_detail'] = 'required';
     }
 
     if ($this->formData['has_allergic']) {
-      $validate['formData.allergic_detail'] = 'required';
+      $this->rules['formData.allergic_detail'] = 'required';
     }
-    $validatedData = $this->validate($validate);
+
+    $validatedData = $this->validate();
 
     $this->education_certificate->storeAs('education_certificates', $this->education_certificate->hashName());
 
